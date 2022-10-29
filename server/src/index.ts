4c564dc6c9f92569b7c827338f65b09c83e59d61
Kawasaki <iduchungho@gmail.com>
2022-10-29 22:48:01 +0700

    chỉnh sửa mô tả file index.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * index.ts file created on 2022-10-29
- * WELLCOME TO index.ts
+ * WELCOME TO index.ts
  * @author Hung, Duc Ho    <https://github.com/iduchungho>
  * @author Phat, Duy Thoi  <https://github.com/duyphatbk>
  * @author Phong, Gia Ngo  <https://github.com/ngogiaphong123>
