436787ecee775ae3f41f6e2a58f7e49bc1801e86
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-11-20 23:14:36 +0700

    Setup file index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,20 +7,51 @@
  * @author Nam, Thanh Nguy <https://github.com/nam7cm>
  */
 
-
-
 /**
  * Required External Modules
  */
-
+import cors, { CorsOptions } from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import deserializeUser from "./middlewares/deserializeUser";
+import ExpressError from "./utils/expressError";
+import log from "./utils/logger";
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 /**
  * App Variables
  */
+const app = express();
+const port = process.env.PORT || 3000;
+const corsOptions: CorsOptions = {
+    credentials: true,
+    origin: true,
+}
+/**
+ *  Middlewares
+ */
+app.use(cors(corsOptions))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(deserializeUser)
+/**
+ * Routes Definitions
+*/
 
 /**
- *  App Configuration
- */
-
-/**
- * Server Activation
- */
+ * Health Check, Error Handling, and Running the Server
+*/
+app.get('/healthcheck', (req: Request, res: Response) => {
+    res.status(StatusCodes.OK).send('ok');
+})
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    return next(new ExpressError('Not Found', StatusCodes.NOT_FOUND))
+})
+app.use((err: ExpressError, req: Request, res: Response) => {
+    const { status = StatusCodes.INTERNAL_SERVER_ERROR, message = 'Something went wrong' } = err
+    res.status(status).send(message)
+})
+app.listen(port, () => {
+    log.info(`Server is listening on port ${port}`)
+})
