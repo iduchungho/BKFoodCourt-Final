41671f27b5921843d9c7bb039da5dc06c083d23f
Kawasaki <iduchungho@gmail.com>
2022-12-15 20:15:07 +0700

    chinh sua 1 xiu css

--- client/src/component/modules/Sucess/Sucess.js
+++ client/src/component/modules/Sucess/Sucess.js
@@ -2,15 +2,17 @@ import './Sucess.css'
 function Sucess() {
     return (
         <div className="Sucess" >
-            <div className="txt1">Bạn đã Đặt hàng thành công</div>
-            <div className="txt2">Cám ơn Quý khách đã mua sắm tại <span>BKFood.com</span>.
-                Đơn hàng của Quý khách đang được xử lý. Mã số đơn hàng của Quý khách là:
-            </div>
-            <div className="code">0169-20221212</div>
-            <div className="txt3">
-                <p>Nhân viên của chúng tôi sẽ sớm liên hệ với Quý khách trong thời gian sớm nhất.</p>
+            <div className='sc__ctn'>
+                <div className="txt1">Bạn đã Đặt hàng thành công</div>
+                <div className="txt2">Cám ơn Quý khách đã mua sắm tại <span>BKFood.com</span>.
+                    Đơn hàng của Quý khách đang được xử lý. Mã số đơn hàng của Quý khách là:
+                </div>
+                <div className="code">0169-20221212</div>
+                <div className="txt3">
+                    <p>Nhân viên của chúng tôi sẽ sớm liên hệ với Quý khách trong thời gian sớm nhất.</p>
 
-                <p>Nếu Quý khách hàng có thắc mắc, xin vui lòng liên hệ với chúng tôi qua số <br></br>hotline <strong>(0252)36 88888</strong></p>
+                    <p>Nếu Quý khách hàng có thắc mắc, xin vui lòng liên hệ với chúng tôi qua số <br></br>hotline <strong>(0252)36 88888</strong></p>
+                </div>
             </div>
         </div>
     )
