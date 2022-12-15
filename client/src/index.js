553ca8907b1a8c6c3ad87e51bd518705860889aa
Kawasaki <iduchungho@gmail.com>
2022-12-15 20:03:45 +0700

    chỉnh sửa scrollbar

--- client/src/index.js
+++ client/src/index.js
@@ -13,6 +13,7 @@ import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
